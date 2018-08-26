5ba36cd9c71e2118a4d8ad6c498bd8572d0fc340
rfpoulos <rfpoulos@outlook.com>
2018-08-26 02:04:07 -0400

    search country and search company by ilike

--- employee.js
+++ employee.js
@@ -29,5 +29,32 @@ employee.get('/companynotes/:id', async (req, res) =>{
     let { id } = req.params;
     let notes = await getCompanyNotes(id);
     res.send(notes);
-})
+});
+
+let getCountries = (country) =>
+    db.query(`
+    SELECT DISTINCT country 
+    FROM companies
+    WHERE country ILIKE $1;
+    `, '%' + country + '%')
+
+employee.get('/countries/:country', async (req, res) => {
+    let { country } = req.params;
+    let countries = await getCountries(country);
+    res.send(countries);
+});
+
+let getCompanyByName = (company) =>
+    db.query(`
+    SELECT *
+    FROM companies
+    WHERE name ILIKE $1;
+    `, '%' + company + '%')
+
+employee.get('/companyname/:company', async (req, res) => {
+    let { company } = req.params;
+    let companies = await getCompanyByName(company);
+    res.send(companies);
+});
+
 module.exports = employee;
